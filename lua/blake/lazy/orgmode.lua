d0e4b4d5edaf9a9b8a48ef2e2bfbd70aece16a3d
blake <Houriganblake@proton.me>
2024-08-21 13:15:26 -0400

    first commit

---
+++ lua/blake/lazy/orgmode.lua
@@ -0,0 +1,20 @@
+return
+{
+  'nvim-orgmode/orgmode',
+  event = 'VeryLazy',
+  ft = { 'org' },
+  config = function()
+    -- Setup orgmode
+    require('orgmode').setup({
+      org_agenda_files = '~/Documents/orgmode/**/*',
+      org_default_notes_file = '~/Documents/orgmode/todos.org',
+    })
+
+    -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
+    -- add ~org~ to ignore_install
+    -- require('nvim-treesitter.configs').setup({
+    --   ensure_installed = 'all',
+    --   ignore_install = { 'org' },
+    -- })
+  end,
+}
